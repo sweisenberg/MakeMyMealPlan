c288aa17f19f436e18bb14db55c01758ec869915
IsaacSherman <sherman1337@gmail.com>
2015-11-10 16:14:47 -0500

    Generated events

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+
 public class Controller {
+    public void makeDinnerFocus(ListView.EditEvent<ListView> tEditEvent) {
+
+    }
+
+    public void makeLunchFocus(ListView.EditEvent<ListView> tEditEvent) {
+    }
+
+    public void addToDinner(ActionEvent actionEvent) {
+    }
+
+    public void addToLunch(ActionEvent actionEvent) {
+    }
+
+    public void addToBreakfast(ActionEvent actionEvent) {
+    }
+
+    public void printLabels(ActionEvent actionEvent) {
+    }
+
+    public void removeAllItems(ActionEvent actionEvent) {
+    }
+
+    public void makeBreakfastFocus(ListView.EditEvent<ListView> tEditEvent) {
+    }
+
+    public void removeFocusedItem(ActionEvent actionEvent) {
+    }
 }
