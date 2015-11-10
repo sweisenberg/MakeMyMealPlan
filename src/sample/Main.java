1e8a100bbadad37f98cd87648f0bad3d998f5672
IsaacSherman <sherman1337@gmail.com>
2015-11-10 15:09:47 -0500

    first commit

---
+++ src/sample/Main.java
@@ -0,0 +1,59 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("groupVis.fxml"));
+        primaryStage.setTitle("Not a title");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /*
+    Load function, core.  Needs touching up for our specific needs:
+
+
+
+     //Reads data in from a file.  Non-numerics are translated as 0, expects comma[space] as delimeter, probably unnecessary
+    public static void load(String file) throws IOException {
+
+        Scanner line, in;
+        in = new Scanner(new File(file));
+        String firstLine = in.nextLine();
+        for (String x : firstLine.split(", ")) labels.add(x);
+        labelToIndex = new Hashtable<String, Integer>(labels.size());
+        for (int i = 0; i < labels.size(); ++i)
+            labelToIndex.put(labels.get(i), i);
+
+        while (in.hasNextLine()) {
+            ArrayList<Double> tempData = new ArrayList<Double>(labels.size());
+            line = new Scanner(in.nextLine());
+            line.useDelimiter(", ");
+            while (line.hasNext()) {
+                if (line.hasNextDouble()) {
+                    tempData.add(line.nextDouble());
+                } else {
+                    tempData.add(0.0);
+                    line.next();
+                }
+            }
+            data.add(tempData);
+
+
+        }
+
+    }
+     */
+}
