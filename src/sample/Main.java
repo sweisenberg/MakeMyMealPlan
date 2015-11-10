39c81d781374cd18e61953cc08d36653e1f78db2
IsaacSherman <sherman1337@gmail.com>
2015-11-10 16:00:59 -0500

    Message

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("groupVis.fxml"));
         primaryStage.setTitle("Not a title");
         primaryStage.setScene(new Scene(root));
-        primaryStage.show();//This is more complicated than it needs to be.
+        primaryStage.show();//This is more complicated than it needs to be.ggg
 
     }
 
